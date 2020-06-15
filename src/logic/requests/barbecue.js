fac5b408d6e136e6a57c827537433ba27d469144
Gosch <dev@gosch.io>
2020-06-15 04:07:10 -0300

    Update legend of barbecue request file

--- src/logic/requests/barbecue.js
+++ src/logic/requests/barbecue.js
@@ -13,39 +13,69 @@ export async function getBarbecues() {
 
 /**
  * Get Barbecue Details
- * 
+ *
  * @param {string} id Barbecue identify
  */
 export async function getBarbecue(id) {
   return await API().get(`/barbecue/${id}`);
 }
 
+/**
+ * Get Members of a Barbecue
+ *
+ * @param {string} id 
+ */
 export async function getBarbecueMembers(id) {
   return await API().get(`/barbecue/${id}/member`);
 }
 
+/**
+ * Create a Barbecue
+ *
+ * @param {limit, offset} data
+ */
 export async function createBarbecue({title, date, suggestedBudget, description, notes}) {
   return await API().post('/barbecue', {
     title, date, suggestedBudget: +suggestedBudget, description, notes
   });
 }
 
+/**
+ * Create a Member to a Barbecue
+ *
+ * @param {barbecue, name, budget, needDrink} data 
+ */
 export async function createMember({barbecue, name, budget, needDrink}) {
   return await API().post(`/barbecue/${barbecue}/member`, {
     name, budget: +budget, needDrink
   });
 }
 
+/**
+ * Update a Member of a Barbecue
+ *
+ * @param {id, barbecue, name, budget, needDrink} data 
+ */
 export async function updateMember({id, barbecue, name, budget, needDrink}) {
   return await API().put(`/barbecue/${barbecue}/member/${id}`, {
     name, budget: +budget, needDrink
   });
 }
 
+/**
+ * Delete a Member of a Barbecue
+ *
+ * @param {id, barbecue} data 
+ */
 export async function deleteMember({id, barbecue}) {
   return await API().delete(`/barbecue/${barbecue}/member/${id}`);
 }
 
+/**
+ * Update a member like Paid
+ *
+ * @param {barbecue, member, paid} data 
+ */
 export async function setMemberPaid({barbecue, member, paid}) {
   return await API().put(`/barbecue/${barbecue}/member/${member}`, {
     paid: !!paid,
